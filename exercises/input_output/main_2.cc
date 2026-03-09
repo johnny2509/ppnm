c833b09489d41758caa3088071c6b7e5c63ec803
Jonathan-Peter Gärtner <jonathan-petergartner@MacBook-Pro-tilhrende-Jonathan-Peter.local>
2026-03-09 12:07:04 +0100

    WIP task 2

---
+++ exercises/input_output/main_2.cc
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <vector>
+#include <cmath>
+#include <string>
+
+double x;
+while( std::cin >> x ){
+	std::cout << x <<" "<< std::sin(x) <<" "<< std::cos(x) << std::endl;
+	}
