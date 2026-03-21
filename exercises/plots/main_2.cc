61a88454f524f8dd8a5757a48f830ce012cbbffb
Jonathan-Peter Gärtner <jonathan-petergartner@MacBook-Pro-tilhrende-Jonathan-Peter.local>
2026-03-21 13:46:14 +0100

    Done with 1-3 tasks, 4 (extra) missing

--- exercises/plots/main_2.cc
+++ exercises/plots/main_2.cc
@@ -2,7 +2,6 @@
 #include <iostream>
 #include <cmath>
 #include <vector>
-#include <numbers>
 #include <utility>
 #include "gnuplot-iostream.h"
 
@@ -15,13 +14,28 @@ double my_sgamma(double x){
     	+ (1.0 / 12) / x - (1.0 / 360) / (x * x * x) + (1.0 / 1260) / (x * x * x * x * x);
 	return std::exp(lnsgamma);
 }
+double stirling(double x){
+	if (x==0) return 0;
+	return std::sqrt(2 * PI * x) * std::pow(x / std::exp(1.0), x);
+}
+
 int main(){
-        std::vector <std::pair <double, double>> curve;
-        for(double x = 0; x <= 3.2; x += 0.05){
-                curve.push_back({x, my_sgamma(x)});
-        }
+        std::vector <std::pair <double, double>> gamma_curve;
+	std::vector <std::pair <double, double>> stirling_curve;
+
+        for(double x = 0.001; x <= 3.2; x += 0.05){
+                gamma_curve.push_back({x, my_sgamma(x + 1)}); // + 1 was added to match wikipedia plot
+        	stirling_curve.push_back({x, stirling(x)});
+	}
+
         Gnuplot gp;
-        gp << "plot '-' with lines title 'Stirling approximation', "
-              "'wiki_fact.dat' with points pt 7 ps 1.5 title 'Wikipedia values'\n";
-        gp.send1d(curve);
+	gp << "set xrange [0:3.2]\n";
+	gp << "set yrange [0:6.5]\n";
+	gp << "set grid\n";
+	gp << "plot "
+	      "'-' with lines lw 2 title 'Gamma(x+1)', "
+              "'-' with lines lw 2 title 'sqrt(2*pi*x)(x/e)^x', "
+              "'wiki_fact.dat' with points pt 7 ps 1.5 title 'n!'\n";
+        gp.send1d(gamma_curve);
+	gp.send1d(stirling_curve);
 }
---
+++ exercises/plots/main_3.cc
@@ -0,0 +1,34 @@
+// Task 3: plot of the gamma-function, lngamma-function, and factorial
+#include <iostream>
+#include <cmath>
+#include <vector>
+#include <utility>
+#include <limits>
+#include "gnuplot-iostream.h"
+
+constexpr double PI = 3.14159265358979324;
+
+double lngamma(double x){
+	if(x<=0) return std::numeric_limits<double>::quiet_NaN();
+	if(x<9) return lngamma(x+1)-std::log(x);
+	return x*std::log(x+1/(12*x-1/x/10))-x+std::log(2*PI/x)/2;
+}
+
+int main(){
+        
+        std::vector <std::pair <double, double>> lngamma_curve;
+
+        for(double x = 0.001; x <= 3.2; x += 0.05){
+                lngamma_curve.push_back({x, lngamma(x+1)});
+        }
+
+        Gnuplot gp;
+        gp << "set xrange [-0.1:3.2]\n";
+        gp << "set yrange [-0.4:2.5]\n";
+        gp << "set grid\n";
+        gp << "plot "
+              "'-' with lines lw 2 title 'lnGamma(x+1)', "
+              "'wiki_fact.dat' using 1:(log($2)) with points pt 7 ps 1.5 title 'ln(n!)'\n"; 
+              // the above line takes log of n! (second column)
+        gp.send1d(lngamma_curve);
+}
