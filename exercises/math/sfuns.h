77b748a4745fb94a493821a69b79163d2aef02ee
Jonathan-Peter Gärtner <jonpet.gae@gmail.com>
2026-05-03 12:14:07 +0200

    Add lngamma function declaration for task 3

--- exercises/math/sfuns.h
+++ exercises/math/sfuns.h
@@ -1,5 +1,5 @@
 #pragma once
 namespace sfuns{
     double fgamma(double x);
-    double lngamma(double x); // added for task 3
+    double lngamma(double x); // added for task 3, Chat GPT Instant 5.3
 }
